ba3a712a9e2b578b4f2a5d00f47c969de61f193b
PabloCuneloJr <samuel.p.cunningham@gmail.com>
2019-11-27 15:30:06 -0500

    Updates

---
+++ precache-manifest.b6d82931cb0e7ead1a63996de3884a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102ef317f18d4ce36bb7fddde6080025",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/index.html"
+  },
+  {
+    "revision": "96881d0d204fbead1e92",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/static/css/main.ba980d47.chunk.css"
+  },
+  {
+    "revision": "8170c32dc9969970ac3f",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/static/js/2.4d4d23c0.chunk.js"
+  },
+  {
+    "revision": "96881d0d204fbead1e92",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/static/js/main.a63920e0.chunk.js"
+  },
+  {
+    "revision": "f0670336aa7a1818e903",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/static/js/runtime-main.99fe52fd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "//github.com/PabloCuneloJr/sam-portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
