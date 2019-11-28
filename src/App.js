e9905f5abbaa240e4e68b392bfe230f21bd83901
PabloCuneloJr <samuel.p.cunningham@gmail.com>
2019-11-27 20:49:54 -0500

    fixed typo with basename (potential)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="">
       <div className="App">
 
       <Route exact path="/" component={Projects} />
